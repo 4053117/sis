473e15799ea864907d47c5543c8c581a0133fb30
4053117 <175152532+4053117@users.noreply.github.com>
2025-04-17 01:22:33 +0200

    Course class

--- Code/sis/crud_sis/models.py
+++ Code/sis/crud_sis/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.IntegerField()
+    is_published = models.BooleanField(default=False)
+    inst_id = models.ManyToManyField(Instructor)
+    content = models.TextField()
