ddb08de9183437500fec0fb1acd809234def2a58
4053117 <175152532+4053117@users.noreply.github.com>
2025-04-17 02:56:13 +0200

    Course interaction pages skeleton

--- Code/sis/crud_sis/views.py
+++ Code/sis/crud_sis/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def course_form(request):
+    pass
+
+def course_list(request):
+    pass
+
+def course_enroll(request):
+    pass
+
+def course_delete(request):
+    pass
