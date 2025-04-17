0d411debb6fb4c4d59c1009c50063d9140cb738e
4053117 <175152532+4053117@users.noreply.github.com>
2025-04-17 08:28:36 +0200

    Latest progress (stuck on dynamic linking)

--- Code/sis/crud_sis/instructor_urls.py
+++ Code/sis/crud_sis/instructor_urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.course_list),
-    path('form/', views.course_form),
-    path('enroll/', views.course_enroll),
-    path('delete/', views.course_delete),
+    path('', views.course_list, name='course_list'),
+    path('<int:id>/', views.course_form, name= 'course_view'),
+    path('<int:id>/enroll/', views.course_enroll, name='course_enroll'),
+    path('<int:id>/delete/', views.course_delete, name='course_delete'),
 ]
--- Code/sis/crud_sis/student_urls.py
+++ Code/sis/crud_sis/student_urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.course_list),
-    path('enroll/', views.course_enroll),
+    path('', views.course_list, name='course_list'),
+    path('<int:id>/enroll/', views.course_enroll, name='course_enroll'),
 ]
