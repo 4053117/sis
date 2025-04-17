70c9c8206add720b3760558264846de74dce1988
4053117 <175152532+4053117@users.noreply.github.com>
2025-04-17 02:00:06 +0200

    Added instructor class & expanded Course class

--- Code/sis/crud_sis/models.py
+++ Code/sis/crud_sis/models.py
@@ -2,11 +2,29 @@ from django.db import models
 
 # Create your models here.
 
+class Instructor(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+    class Meta:
+        ordering = ['last_name']
+        permissions = []
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 class Course(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     description = models.TextField()
     price = models.IntegerField()
     is_published = models.BooleanField(default=False)
-    inst_id = models.ManyToManyField(Instructor)
     content = models.TextField()
+    instructors = models.ManyToManyField(Instructor)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
